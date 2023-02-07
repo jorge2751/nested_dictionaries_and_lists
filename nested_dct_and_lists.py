a39f44d396efceea2519a211e61c2dd05ffc33c0
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-02-06 16:54:04 -0800

    First Commit

---
+++ nested_dct_and_lists.py
@@ -0,0 +1,62 @@
+#1
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+print(x)
+
+students[0]['last_name'] = 'Bryant'
+print(students[0])
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory['soccer'])
+
+z[0]['y'] = 30
+print(z)
+
+#2
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+
+def iterateDictionary(some_list):
+    for dct in some_list:
+        for key in dct:
+            print(key,'-',dct[key])
+
+iterateDictionary(students)
+
+#3
+def iterateDictionary2(key_name, some_list):
+    for dct in some_list:
+        for key in dct:
+            if key == key_name:
+                print(dct[key])
+
+iterateDictionary2('last_name', students)
+
+#4
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key in some_dict:
+        print(len(some_dict[key]), key.upper())
+        for value in some_dict[key]:
+            print(value)
+
+printInfo(dojo)
